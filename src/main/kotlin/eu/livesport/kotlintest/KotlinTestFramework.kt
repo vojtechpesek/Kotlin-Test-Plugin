40ce1bae44c6fd0da11bbdead6b384284c20e38e
vojtech.pesek <vojtech.pesek@livesport.eu>
2022-10-14 15:36:00 +0200

    add search for setup teardown methods

--- src/main/kotlin/eu/livesport/kotlintest/KotlinTestFramework.kt
+++ src/main/kotlin/eu/livesport/kotlintest/KotlinTestFramework.kt
@@ -1,33 +1,27 @@
 package eu.livesport.kotlintest
 
+import com.intellij.codeInsight.AnnotationUtil
+import com.intellij.codeInsight.intention.AddAnnotationFix
+import com.intellij.execution.JUnitBundle
+import com.intellij.execution.junit.JUnitUtil
 import com.intellij.ide.fileTemplates.FileTemplateDescriptor
+import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.lang.Language
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.IconLoader
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElement
-import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.*
+import com.intellij.psi.codeStyle.JavaCodeStyleManager
 import com.intellij.testIntegration.TestFramework
-import org.jetbrains.kotlin.asJava.classes.KtLightClass
-import org.jetbrains.kotlin.asJava.classes.KtUltraLightClass
 import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 import javax.swing.Icon
 
 class KotlinTestFramework : TestFramework {
 
     override fun getDefaultSuperClass(): String = ""
-
-    private fun isUnderTestSources(clazz: PsiClass): Boolean {
-        val psiFile = clazz.containingFile
-        val vFile = psiFile.virtualFile ?: return false
-        return ProjectRootManager.getInstance(clazz.project).fileIndex.isInTestSourceContent(vFile)
-    }
 
     /**
      * Returns true if the given element could be a test class.
@@ -35,17 +29,11 @@ class KotlinTestFramework : TestFramework {
      * not test classes - such as elements in src/main or elements that are not class elements.
      */
     override fun isPotentialTestClass(clazz: PsiElement): Boolean {
-        return clazz is PsiClass && isUnderTestSources(clazz)
+        return clazz is PsiClass && clazz.isUnderTestSources()
     }
 
     override fun isTestClass(clazz: PsiElement): Boolean {
-        return false
-//      return when (clazz) {
-//         is KtUltraLightClass -> clazz.kotlinOrigin.isSpec()
-//         is KtLightClass -> clazz.kotlinOrigin?.isSpec() ?: false
-//         is KtClass -> clazz.isSpec()
-//         else -> false
-//      }
+        return isPotentialTestClass(clazz)
     }
 
     override fun getName(): String = "Kotlin test"
@@ -54,26 +42,95 @@ class KotlinTestFramework : TestFramework {
 
     override fun getLibraryPath(): String? = null
 
-    override fun findOrCreateSetUpMethod(clazz: PsiElement): PsiElement? = null
+    override fun findSetUpMethod(element: PsiElement): PsiElement? {
+        if (element is PsiClass) {
+            return element.methods.filterNotNull().find {
+                AnnotationUtil.isAnnotated(it, BEFORE_ANNOTATION_NAME, 0)
+            }
+        }
+        return null
+    }
 
-    // kotest does not use method as tests
-    override fun isIgnoredMethod(element: PsiElement?): Boolean = false
+    override fun findTearDownMethod(element: PsiElement): PsiElement? {
+        if (element is PsiClass) {
+            return element.methods.filterNotNull().find {
+                AnnotationUtil.isAnnotated(it, AFTER_ANNOTATION_NAME, 0)
+            }
+        }
+        return null
+    }
 
-    override fun findSetUpMethod(clazz: PsiElement): PsiElement? = null
+    override fun findOrCreateSetUpMethod(element: PsiElement): PsiElement? {
+        if (element is PsiClass) {
+            return findOrCreateSetUpMethod(element)
+        }
+        return null
+    }
 
-    override fun findTearDownMethod(clazz: PsiElement): PsiElement? = null
+    private fun findOrCreateSetUpMethod(
+        clazz: PsiClass,
+    ): PsiMethod? {
+        var method = findSetUpMethod(clazz)
+        if (method != null) return method as? PsiMethod
+        val manager = clazz.manager
+        val factory = JavaPsiFacade.getInstance(manager.project).elementFactory
+        method = createSetUpPatternMethod(factory)
+        val existingMethod = clazz.findMethodBySignature(method, false)
+        if (existingMethod != null) {
+            if (AnnotationUtil.isAnnotated(existingMethod, BEFORE_ANNOTATION_NAME, 0)) return existingMethod
+            val exit =
+                if (ApplicationManager.getApplication().isUnitTestMode) Messages.OK else Messages.showOkCancelDialog(
+                    JUnitBundle.message("create.setup.dialog.message", "@BeforeTest"),
+                    JUnitBundle.message("create.setup.dialog.title"),
+                    Messages.getWarningIcon()
+                )
+            if (exit == Messages.OK) {
+                AddAnnotationFix(BEFORE_ANNOTATION_NAME, existingMethod).invoke(
+                    existingMethod.project,
+                    null,
+                    existingMethod.containingFile
+                )
+                return existingMethod
+            }
+        }
+        val testMethod = JUnitUtil.findFirstTestMethod(clazz)
+        method = if (testMethod != null) {
+            clazz.addBefore(method, testMethod) as PsiMethod
+        } else {
+            clazz.add(method) as PsiMethod
+        }
+        JavaCodeStyleManager.getInstance(manager.project).shortenClassReferences(method)
+        return method
+    }
 
+    private fun createSetUpPatternMethod(factory: JVMElementFactory): PsiMethod {
+        val template = FileTemplateManager.getDefaultInstance().getCodeTemplate(
+            setUpMethodFileTemplateDescriptor.fileName
+        )
+        val templateText = StringUtil.replace(StringUtil.replace(template.text, "\${BODY}\n", ""), "\${NAME}", "setUp")
+        return factory.createMethodFromText(templateText, null)
+    }
 
-    // kotest does not use method as tests
-    override fun isTestMethod(element: PsiElement?): Boolean = false
+    override fun isTestMethod(element: PsiElement?): Boolean = testMethod(element)
 
-    // kotest does not use method as tests
-    override fun isTestMethod(element: PsiElement?, checkAbstract: Boolean): Boolean = false
+    override fun isTestMethod(element: PsiElement?, checkAbstract: Boolean): Boolean = testMethod(element)
+
+    private fun testMethod(element: PsiElement?): Boolean {
+        return when (element) {
+            is KtNamedFunction -> element.annotationEntries.any { it.shortName?.identifier == "Test" || it.shortName?.identifier == "kotlin.test.Test" }
+            else -> false
+        }
+    }
 
     override fun getSetUpMethodFileTemplateDescriptor(): FileTemplateDescriptor = FileTemplateDescriptor("Kotlin Test After.kt")
     override fun getTearDownMethodFileTemplateDescriptor(): FileTemplateDescriptor = FileTemplateDescriptor("Kotlin Test Before.kt")
 
     override fun getTestMethodFileTemplateDescriptor(): FileTemplateDescriptor = FileTemplateDescriptor("Kotlin Test Method.kt")
+
+    override fun isIgnoredMethod(element: PsiElement?): Boolean {
+        val testMethod = if (element is PsiMethod) JUnitUtil.getTestMethod(element) else null
+        return testMethod != null && AnnotationUtil.isAnnotated(testMethod, IGNORE_ANNOTATION_NAME, 0)
+    }
 
     override fun isLibraryAttached(module: Module): Boolean {
         return true
---
+++ src/main/kotlin/eu/livesport/kotlintest/KotlinTestUtil.kt
@@ -0,0 +1,5 @@
+package eu.livesport.kotlintest
+
+const val IGNORE_ANNOTATION_NAME = "kotlin.test.Ignore"
+const val BEFORE_ANNOTATION_NAME = "kotlin.test.BeforeTest"
+const val AFTER_ANNOTATION_NAME = "kotlin.test.AfterTest"
